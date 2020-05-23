b0595171204c36d2583e356c688accaa91c5282b
ngocthoha <tho.ha_2502@hcmut.edu.vn>
2020-05-23 10:48:56 +0700

    update

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <div class="login-form">
       <label for="email">Email</label>
-      <input id="email" v-model="email" placeholder="Your email" />
+      <input id="email" v-model="email" placeholder="Enter email" />
 
       <label for="password">Password</label>
-      <input id="password" v-model="password" placeholder="Your password" />
+      <input id="password" v-model="password" placeholder="Enter password" />
       
       <button class="login-button" v-on:click="login">Login</button>
     </div>
@@ -17,7 +17,7 @@ import AxiosService from '../services/axios-service'
 
 export default {
   name: "LoginForm",
-  data: function () {
+  data() {
     return {
       email: "",
       password: ""
