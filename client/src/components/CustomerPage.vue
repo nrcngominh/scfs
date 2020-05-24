092c4787a28d32e0d3482eef951cfbd558e57d66
ngocthoha <tho.ha_2502@hcmut.edu.vn>
2020-05-25 03:01:32 +0700

    update

---
+++ client/src/components/CustomerPage.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="container">
+
+    <h1>{{menu}}</h1>
+    <div class="mb-3 mt-3">
+        <button @click="logout">Logout</button>
+    </div>
+
+    <div class="row">
+        <div class="tab-content col-xl-12" id="myTabContent">
+            <div class="tab-pane fade active show" id="dinner" role="tabpanel" aria-labelledby="dinner-tab">
+                <div class="row" >
+                    <div class="col-md-6" v-for="food in foods" :key="food.name">
+                        <div class="single_menu_list">
+                            <img src="https://images.foody.vn/res/g10/98048/prof/s576x330/foody-upload-api-foody-mobile-com-190311132057.jpg" alt="">
+                            <div class="menu_content" >
+                                <h4>{{ food.name }}  <span>{{ food.price }}</span></h4>
+                                <p>{{ food.description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import AxiosService from '../services/axios-service'
+
+export default {
+  name: "CustomerPage",
+  data() {
+    return {
+        menu: "CustomerPage",
+        foods: [],
+        name: "",
+        price: 0,
+        description: ""
+    }
+  },
+  beforeCreate() {
+    document.body.className = "user";
+  },
+  methods: {
+    logout() {
+      this.$router.push('/')
+    },
+    async add() {
+      try {
+        const newFood = {
+          name: this.name,
+          price: this.price,
+          description: this.description
+        }
+        const res = await AxiosService.post('/food', newFood)
+        console.log(res.data)
+        if (res.data.status == 'success') {
+          newFood._id = res.data._id
+          this.foods.push(newFood)
+        } else {
+          alert('failed')
+        }
+      } catch (err) { 
+        console.log(err)
+      }
+    },
+    async order() {
+
+    }
+  }, 
+  async mounted() {
+    const res = await AxiosService.get('/food')
+    this.foods = res.data.foods
+  }
+}
+</script>
+<style>
+
+#myTabContent {
+	margin-top: 50px;
+}
+.menu_tab {
+	width: auto;
+	margin: 0 auto;
+}
+.nav-tabs .nav-item {
+	margin-bottom: -1px;
+}
+.nav-item a {
+	color: #333;
+}
+.menu_tab .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
+	color: white;
+	background-color: #FF5E18;
+	border-color: #FF5E18;
+}
+.single_menu_list {
+	position: relative;
+	padding-left: 190px;
+	margin-bottom: 110px;
+}
+
+.single_menu_list img {
+	max-width: 30%;
+	position: absolute;
+	left: 0px;
+	top: 0;
+	border: 1px solid #ddd;
+	padding: 3px;
+	/* border-radius: 50%; */
+	transition: .4s
+}
+
+
+.single_menu_list h4 {
+	font-size: 25px;
+	border-bottom: 1px dashed #333;
+	padding-bottom: 15px;
+	margin-bottom: 10px;
+}
+
+.single_menu_list h4 span {
+	float: right;
+	font-weight: bold;
+	color: #FF5E18;
+	font-style: italic;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,28 +3,33 @@ import VueRouter from 'vue-router'
 import LoginForm from '../components/LoginForm'
 import ErrorPage from '../components/ErrorPage'
 import ManagerPage from '../components/ManagerPage'
+import CustomerPage from '../components/CustomerPage'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'LoginForm',
-      component: LoginForm
-    },
-    {
-      path: '/error',
-      name: 'ErrorPage',
-      component: ErrorPage
-    },
-    {
-      path: '/admin',
-      name: 'ManagerPage',
-      component: ManagerPage
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'LoginForm',
+            component: LoginForm
+        },
+        {
+            path: '/error',
+            name: 'ErrorPage',
+            component: ErrorPage
+        },
+        {
+            path: '/admin',
+            name: 'ManagerPage',
+            component: ManagerPage
+        },
+        {
+            path: '/user',
+            name: 'CustomerPage',
+            component: CustomerPage
+        }
+    ]
 })
 
 export default router;
