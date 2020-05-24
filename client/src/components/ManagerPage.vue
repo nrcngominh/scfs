3b43ddea8f8e1e281285fbc77154a7a0bdab5ff7
Richard <nrcngominh@gmail.com>
2020-05-24 21:24:47 +0700

    update

--- client/src/components/ManagerPage.vue
+++ client/src/components/ManagerPage.vue
@@ -5,7 +5,7 @@
         <button @click="logout">Logout</button>
     </div>
 
-    <form>
+    <div>
       <div class="input-group input-group-sm mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text" id="inputGroup-sizing-sm">Name</span>
@@ -24,8 +24,8 @@
         </div>
         <input type="text" class="form-control" v-model="description" aria-label="Description" aria-describedby="inputGroup-sizing-sm">
       </div>
-      <button type="submit" class="btn btn-primary" :click="add">Add</button>
-    </form>
+      <button class="btn btn-primary" @click="add">Add</button>
+    </div>
 
     <div class="food-container">
         <div v-for="food in foods" :key="food.name">
@@ -62,7 +62,8 @@ export default {
     },
     async add() {
       try {
-        const res = await AxiosService.post('/add/', {
+        console.log("Adding food")
+        const res = await AxiosService.post('/food', {
           name: this.name,
           price: this.price,
           description: this.description
--- server/src/api/routers/food-router.js
+++ server/src/api/routers/food-router.js
@@ -4,8 +4,8 @@ import FoodService from '../../services/food-service'
 const FoodRouter = Router()
 
 FoodRouter.post('/', async (req, res) => {
-  const isSuccess = await FoodService.add(req.body.name, req.body.price, req.body.description)
   console.log(req.body)
+  const isSuccess = await FoodService.add(req.body.name, req.body.price, req.body.description)
   res.send({
     status: isSuccess ? 'success' : 'failed'
   })
