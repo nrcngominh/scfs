2fba054a56981faaa8a3f46c16ddd4f10dc34a56
ngocthoha <tho.ha_2502@hcmut.edu.vn>
2020-05-24 17:18:00 +0700

    update

--- client/src/components/ManagerPage.vue
+++ client/src/components/ManagerPage.vue
@@ -4,12 +4,27 @@
     <div class="abc">
         <button @click="logout">Logout</button>
     </div>
-    </div>
+    <div id="app">
+    <ul class="collection">
+        <li v-for="item in foods" :key="item.name">
+          <ul>{{item.name}}</ul>
+        </li>    
+    </ul>
+</div>
 </template>
 
 <script>
-//import AxiosService from '../services/axios-service'
-
+import axios from '../services/axios-service';
+new Vue({
+  el: '#v-for-object',
+  data: {
+    object: {
+      title: 'How to do lists in Vue',
+      author: 'Jane Doe',
+      publishedAt: '2016-04-10'
+    }
+  }
+})
 export default {
   name: "ManagerPage",
   data() {
@@ -23,7 +38,14 @@ export default {
   methods: {
     logout() {
       this.$router.push('/')
-    }
+    },
+  },
+  mounted() { 
+      axios.get('/food')
+      .then((res) => {
+        console.log(res.data);  
+        this.foods = res.data;
+      })
   }
 }
 </script>
