8eb45159f3721a40f75e6b14edb65c4afd91ad90
Richard <nrcngominh@gmail.com>
2020-05-24 23:54:06 +0700

    update

--- server/src/api/routers/index.js
+++ server/src/api/routers/index.js
@@ -1,9 +1,16 @@
-import Router from 'express'
+import express from 'express'
 import LoginRouter from './login-router'
 import RegisterRouter from './register-router'
 import FoodRouter from './food-router'
+import path from 'path'
 
-const BaseRouter = Router()
+const BaseRouter = express.Router()
+
+// Redirect homepage
+const publicDir = path.join(process.env.PWD, '../client/dist')
+console.log(publicDir);
+
+BaseRouter.use('/', express.static(publicDir))
 BaseRouter.use('/login', LoginRouter)
 BaseRouter.use('/register', RegisterRouter)
 BaseRouter.use('/food', FoodRouter)
