0b8a952151c4b070420625e3c82b15799e687e21
Richard <nrcngominh@gmail.com>
2020-05-25 01:32:40 +0700

    update

--- client/src/services/axios-service.js
+++ client/src/services/axios-service.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api/" : "/api"
+const baseURL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api/' : '/api'
 
 const AxiosService = axios.create({
-  baseURL: baseURL
+    baseURL: baseURL
 })
 
 export default AxiosService
