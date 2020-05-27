db83136a7ab468e64e87c2aac9f1fde9500ba8c2
Richard <nrcngominh@gmail.com>
2020-05-27 18:12:03 +0700

    update

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -120,175 +120,18 @@ export default {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
-html,
-div,
-span,
-ul,
-li,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-a,
-label {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    vertical-align: baseline
-}
-
-section {
-    display: block
-}
-
-* {
-    box-sizing: border-box
-}
-
-html {
-    height: 100%
-}
-
-ul {
-    list-style: none
-}
-
-a {
-=======
 #_login {
->>>>>>> ngoc
     color: #EB1510;
     text-decoration: underline;
 }
 
 #_login:hover {
     cursor: pointer;
-<<<<<<< HEAD
-    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    margin: 0;
-    outline: none
-}
-
-img {
-    max-width: 100%;
-    vertical-align: bottom
-}
-
-input {
-    outline: none;
-}
-
-.tabs {
-    display: -ms-flexbox;
-    display: flex;
-    margin-top: 24px;
-}
-
-.tabs__item {
-    font-size: 16px;
-}
-
-.tabs__item:not(:last-child) {
-    margin-right: 16px;
-}
-
-.tabs__item.is-active .tabs__number {
-    background-color: #11a2b8
-}
-
-.tabs__item.is-active .tabs__title {
-    display: inline
-}
-
-.tabs__item.is-active~.tabs__item .tabs__number {
-    background-color: rgba(130, 138, 143, 0.3)
-}
-
-.tabs__number {
-    display: inline-block;
-    width: 1.5em;
-    height: 1.5em;
-    border-radius: 50%;
-    background-color: #162945;
-    color: #FFF;
-    line-height: 1.5;
-    text-align: center
-}
-
-.tabs__title {
-    display: none;
-    margin-left: 8px
-}
-
-@media (min-width: 1224px) {
-    .tabs__title {
-        display: inline;
-        margin-left: 10px
-    }
-}
-
-@media (min-width: 801px) {
-    .tabs {
-        margin-top: 16px
-    }
-    .tabs__item {
-        font-size: 12px
-    }
-    .tabs__item:not(:last-child) {
-        padding-bottom: 0
-    }
-    .tabs__content {
-        padding-bottom: 0
-    }
-}
-
-.signup__circleMap {
-    position: absolute;
-    top: 50%;
-    overflow: hidden;
-    transform: translateY(-50%);
-    border-radius: 50%;
-    background-color: #162945;
-    background-image: linear-gradient(160deg, rgba(22, 41, 69, 0) 0%, #162945 85%), url(/assets/backgrounds/signup-a24f239290c3a2fff00736fb0f86a9584f38fcf7c1175a2c23c76f48f79c27a3.svg);
-    background-repeat: no-repeat;
-    background-position: right center;
-    background-size: auto
-}
-
-@media (min-width: 1632px) {
-    .tabs {
-        margin-top: 28px;
-    }
-}
-
-@media (min-width: 1632px) {
-    .signup__circleMap {
-        left: -1300px;
-    }
-}
-
-@media (min-width: 1224px) {
-    .signup__circleMap {
-        left: -1400px;
-    }
-}
-
-@media (min-width: 801px) {
-    .form__label,
-    h1 {
-        color: #162945;
-=======
 }
 
 @media (min-width: 801px) {
     body {
         color: #162945
->>>>>>> ngoc
     }
 }
 
@@ -327,30 +170,6 @@ input {
         padding-left: 16px
     }
 }
-<<<<<<< HEAD
-
-.grid-cell--col1 {
-    flex: 0 0 8.33333%;
-    max-width: 8.33333%
-}
-
-.grid-cell--col5 {
-    flex: 0 0 41.66667%;
-    max-width: 41.66667%
-}
-
-.grid-cell--col10 {
-    flex: 0 0 83.33333%;
-    max-width: 83.33333%
-}
-
-.grid-cell--col12 {
-    flex: 0 0 100%;
-    max-width: 100%
-}
-
-=======
->>>>>>> ngoc
 @media (max-width: 1224px) {
     .grid-cell--col5--desktop-s {
         flex: 0 0 41.66667%;
@@ -416,46 +235,6 @@ input {
     font-size: 16px
 }
 
-<<<<<<< HEAD
-.button.isRed {
-    background-color: #eb1510;
-    color: #ffffff
-}
-
-.signup__form {
-    width: 100%
-}
-
-.form__link {
-    color: #ffffff;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 16px
-}
-
-.form__label,
-.form__input {
-    width: 100%
-}
-
-.form__field {
-    position: relative;
-    width: 100%;
-    margin-top: 24px;
-    line-height: 20px
-}
-
-.form__label {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-align: center;
-    align-items: center;
-    margin-bottom: 8px;
-    font-family: "Montserrat", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 16px
-=======
 .tabs__item:not(:last-child) {
     margin-right: 16px
 }
@@ -469,7 +248,6 @@ input {
     color: #FFF;
     line-height: 1.5;
     text-align: center
->>>>>>> ngoc
 }
 
 .tabs__title {
@@ -508,75 +286,17 @@ input {
     }
 }
 
-<<<<<<< HEAD
-@media (min-width: 801px) {
-    .signup__wrapper {
-        background-color: #ffffff
-    }
-    .signup__circleMap {
-        left: -1400px;
-        width: 2000px;
-        height: 2000px;
-    }
-}
-
-.u-width--100 {
-    width: 100%
-}
-
-.u-p0 {
-    padding: 0;
-}
-
-.u-mt8 {
-    margin-top: 8px;
-}
-
-.u-mt16 {
-    margin-top: 16px;
-}
-
-.u-mb16 {
-    margin-bottom: 16px;
-}
-
-.tabs__footer {
-    padding-top: 32px;
-    width: 100%
-}
-
-.desktop-s,
-.desktop-l {
-    display: block;
-}
-=======
 @media (min-width: 1632px) {
     .tabs {
         margin-top: 28px
     }
 }
->>>>>>> ngoc
 
 @media (max-width: 800px) {
     .desktop-s {
         display: none
     }
 }
-<<<<<<< HEAD
-
-@media (min-width: 1632px) {
-    .grid-cell--col1--desktop-l {
-        flex: 0 0 8.33333%;
-        max-width: 8.33333%;
-    }
-}
-
-.u-align-center {
-    -ms-flex-align: center;
-    align-items: center
-}
-=======
->>>>>>> ngoc
 
 @media (max-width: 1224px) {
     .desktop {
@@ -726,8 +446,6 @@ input {
     font-weight: 600;
     font-style: normal
 }
-<<<<<<< HEAD
-=======
 
 @font-face {
     font-family: "Montserrat";
@@ -759,5 +477,4 @@ input {
     font-weight: 500;
     font-style: normal
 }
->>>>>>> ngoc
 </style>
