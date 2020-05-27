62286bb14eeec75f95bed16518224e8ee0fa0e8e
Richard <nrcngominh@gmail.com>
2020-05-27 17:38:53 +0700

    update

--- server/src/services/register-service.js
+++ server/src/services/register-service.js
@@ -1,10 +1,14 @@
 import AccountModel from '../models/account-model'
+import bycript from 'bcrypt'
 
 export default {
   async register (email, password) {
+
+    const hashedPassword = bycript.hashSync(password, 10);
     return await AccountModel.create({
       email: email,
-      password: password
+      password: hashedPassword,
+      type: 'customer'
     })
   }
 }
