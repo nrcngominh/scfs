897f360011dd55ddd117c26ecf0ba8dfca0b8289
Richard <nrcngominh@gmail.com>
2020-05-27 15:13:58 +0700

    update

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        Footer
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+.wrapper {
+    background-color: purple;
+    height: 100vh;
+    overflow: auto;
+}
+</style>
---
+++ client/src/components/IntroSlide.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="wrapper">
+    IntroSlide
+</div>
+</template>
+
+<script>
+    export default{
+        name: "IntroSlide"
+    }
+</script>
+
+<style scoped>
+.wrapper {
+    background-color: lightblue;
+}
+</style>
