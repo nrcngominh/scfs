a534b4e42a57d7acf0fcc1d12a6a6b08bad725ab
Richard <nrcngominh@gmail.com>
2020-06-03 10:26:17 +0700

    fix client axios url

--- client/src/services/axios-service.js
+++ client/src/services/axios-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = process.env.NODE_ENV === 'development'
-    ? `${process.env.HOST}:3000` : '/'
+    ? 'http://localhost:3000' : '/'
 
 const AxiosService = axios.create({
     baseURL: baseURL
