0c3441fd74b37b165c3635af46edaf7f5e76b504
Richard <nrcngominh@gmail.com>
2020-06-10 20:30:01 +0700

    hotfix image method

--- server/src/models/food-model.js
+++ server/src/models/food-model.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const FoodSchema = mongoose.Schema({
   name: String,
   price: Number,
-  description: String
+  description: String,
+  img: String
 })
 const FoodModel = mongoose.model('Food', FoodSchema, 'food')
 
