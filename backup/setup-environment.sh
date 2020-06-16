fbd270a238507bcdc79a3fd3482cbfbf1e98d35e
Ngoc <nrcngominh@gmail.com>
2020-06-16 22:41:36 +0700

    Update readme

--- backup/setup-environment.sh
+++ backup/setup-environment.sh
@@ -8,10 +8,8 @@ mongorestore -d sfcs localdb/sfcs
 
 # Create folder for serving images
 sudo mkdir -p /web/sfcs
-sudo chmod -r $USER:$USER /web/sfcs
-ln -sf images /web/sfcs/images
-chmod 755 /web/sfcs/images
-chmod 644 /web/sfcs/images/*
+sudo chown -R $USER:$USER /web/sfcs
+ln -sf $(pwd)/images /web/sfcs/images
 
 # Restore NGINX configuration
 sudo cp sfcs-nginx.conf /etc/nginx/sites-enabled/default.conf
