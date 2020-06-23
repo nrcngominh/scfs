99b70a83dfc0e6cd6827d06b1296caac1f2b776c
Son <sonvnj1@gmail.com>
2020-06-23 13:15:04 +0700

    el to vn

--- client-web/src/pages/CheckoutPage.vue
+++ client-web/src/pages/CheckoutPage.vue
@@ -12,10 +12,10 @@
       </div>
       <div class="basket-labels">
         <ul>
-          <li class="item item-heading">Item</li>
-          <li class="price1">Price</li>
-          <li class="quantity">Quantity</li>
-          <li class="subtotal1">Subtotal</li>
+          <li class="item item-heading">Vật phẩm</li>
+          <li class="price1">Giá</li>
+          <li class="quantity">Số lượng</li>
+          <li class="subtotal1">Thành tiền</li>
         </ul>
       </div>
 
@@ -49,12 +49,12 @@
     </div>
     <aside>
       <div class="summary">
-        <div class="summary-total-items"><span class="total-items"></span> Items in your Bag</div>
+        <div class="summary-total-items"><span class="total-items"></span>Giỏ hàng</div>
         <div class="summary-subtotal">
-          <div class="subtotal-title">Subtotal</div>
+          <div class="subtotal-title">Tạm tính</div>
           <div class="subtotal-value final-value" id="basket-subtotal">{{totalMoney}}</div>
           <div class="summary-promo hide">
-            <div class="promo-title">Promotion</div>
+            <div class="promo-title">Giảm giá</div>
             <div class="promo-value final-value" id="basket-promo"></div>
           </div>
         </div>
@@ -71,16 +71,16 @@
 
 
         <div class="summary-promotional">
-          <label for="promo-code">Enter a promotional code</label>
+          <label for="promo-code">Nhập mã giảm giá</label>
           <input id="promo-code" v-model="promoCode" type="text" name="promo-code" maxlength="5" class="promo-code-field">
-          <button class="promo-code-cta" @click="applyPromotionCode()">Apply</button>        
+          <button class="promo-code-cta" @click="applyPromotionCode()">Xác nhận</button>        
         </div>
         <div class="summary-total">
-          <div class="total-title">Total</div>
+          <div class="total-title">Thành tiền</div>
           <div class="total-value final-value" id="basket-total">{{totalMoneyAfterDiscount()}}</div>
         </div>
         <div class="summary-checkout">
-          <button class="checkout-cta" @click="$router.push('/payment')">Confirm Order</button>
+          <button class="checkout-cta" @click="$router.push('/payment')">Xác nhận đơn hàng</button>
         </div>
       </div>
 
@@ -329,7 +329,7 @@ label {
   padding: 0.5rem;
   text-transform: uppercase;
   transition: all 0.2s linear;
-  width: 66%;
+  width: 60%;
   -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
   -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
   -o-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
