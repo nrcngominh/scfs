84ec7f4639ee8e15c28aa6638a87f7d26ee19b84
Ngoc <nrcngominh@gmail.com>
2020-06-25 19:41:48 +0700

    fix docker

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ sudo docker build -t sfcs/mongo:v1 ./database
 sudo docker build -t sfcs/server-api:v1 ./server-api
 sudo docker build -t sfcs/server-payment:v1 ./server-payment
 sudo docker build -t sfcs/client-web:v1 ./client-web
+
 sudo docker-compose down
 sudo docker-compose up
