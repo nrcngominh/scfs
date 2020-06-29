edc7815b84dcd5121ee94ce989917cbb7950b6d2
Ngoc <nrcngominh@gmail.com>
2020-06-29 22:48:01 +0700

    lint

--- client-web/src/layouts/MainLayout.vue
+++ client-web/src/layouts/MainLayout.vue
@@ -58,16 +58,14 @@ export default {
         foods = foods.concat(obj.foods);
       });
       this.$store.commit("setFoods", foods);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
 
     try {
       const cartRes = await this.$http.get("/api/cart");
       this.$store.commit("setCart", cartRes.data);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   }
--- client-web/src/main.js
+++ client-web/src/main.js
@@ -1,36 +1,20 @@
 import Vue from 'vue'
 import VueCookies from 'vue-cookies'
 import App from './App'
-import store from './store'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from 'socket.io-client'
 import VueQrCodeComponent from 'vue-qrcode-component'
-import axios from 'axios'
 
-import './assets/scss/index.scss'
-  
-
-axios.defaults.baseURL = process.env.VUE_APP_DOMAIN
-Vue.prototype.$http = axios
-
-import router from './router'
-
-//Add fontawesome
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faFacebookF, faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import '@fortawesome/fontawesome-free/css/all.css'
-// Add the specific imported icons
-// library.add(faFacebookF, faTwitter, faGithub, faLinkedin)
-
-// // Enable the FontAwesomeIcon component globally
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.component('qr-code', VueQrCodeComponent)
 Vue.config.productionTip = false;
-
 Vue.use(VueCookies)
 Vue.$cookies.config('1d')
+
+import './assets/scss/index.scss'
+Vue.component('qr-code', VueQrCodeComponent)
+
+import axios from 'axios'
+axios.defaults.baseURL = process.env.VUE_APP_DOMAIN
+Vue.prototype.$http = axios
 
 Vue.use(new VueSocketIO({
   debug: true,
@@ -41,6 +25,9 @@ Vue.use(new VueSocketIO({
     mutationPrefix: 'SOCKET_'
   }
 }))
+
+import store from './store'
+import router from './router'
 
 new Vue({
   store,
