94153401c3a9027a4d9da882837ad3968b7fa2fe
Ngoc <nrcngominh@gmail.com>
2020-07-04 23:37:12 +0700

    update db

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 sudo docker build -t sfcs/database:v1.dev ./database
 sudo docker build -t sfcs/server-api:v1.dev ./server-api
-sudo docker build -t sfcs/server-payment:v1.dev ./server-payment
 sudo docker build -t sfcs/client-web:v1.dev ./client-web
 
 sudo docker-compose down
