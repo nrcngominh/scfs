de4824a310f3d6ede8a7c138c84ce494ac2a7fdc
Son <sonvnj1@gmail.com>
2020-07-09 23:10:14 +0700

    fix

--- client-admin/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import LoginPage from "../pages/LoginPage";
-import Dashboard from "../pages/Dashboard.vue";
-import ProfilePage from "../pages/ProfilePage.vue";
-import NotificationPage from "../pages/NotificationPage.vue";
-import FoodPage from "../pages/FoodPage.vue"
-import AboutPage from "../pages/AboutPage.vue";
-
-export { LoginPage, Dashboard, ProfilePage, NotificationPage, AboutPage, FoodPage };
--- client-machine/app/src/main/java/com/example/sfcs_hcmut/common/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.sfcs_hcmut.common;
-
-public class Config {
-    public static String SERVER_URL = "http://192.168.1.242/";
-//    public static String SERVER_URL = "https://sfcs-hcmut.duckdns.org/";
-}
